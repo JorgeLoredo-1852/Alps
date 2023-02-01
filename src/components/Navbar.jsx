cb2bcbee3a27caccfa28d96acd5ef1c50b74bfcf
JorgeLoredo-1852 <jorgeloremele@gmail.com>
2023-01-31 22:21:26 -0600

    Styled navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,22 +14,11 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { makeStyles } from "@material-ui/core/styles";
 import { useTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
-
-
-import SwipeableDrawer from '@mui/material/SwipeableDrawer';
 import Button from '@mui/material/Button';
-import Drawer from '@mui/material/Drawer';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const theme = createTheme({
-    status: {
+    status: { 
       danger: '#e53e3e',
     },
     palette: {
@@ -47,13 +36,6 @@ const theme = createTheme({
     },
   });
 
-  const useStyles = makeStyles(theme => ({
-    customHoverFocus: {
-      "&:hover, &.Mui-focusVisible": { backgroundColor: "#CCD9F0" },
-      "&:hover > *":{color:"#5364A7"}
-    }
-  }));
-
 export default function Navbar() {
     const themeM = useTheme();
 
@@ -61,8 +43,7 @@ export default function Navbar() {
     const downMd = useMediaQuery(themeM.breakpoints.down('md'));
     const downSm = useMediaQuery(themeM.breakpoints.down('sm'));
     const [showDrawer, setShowDrawer] = useState(null)
-
-    const classes = useStyles();
+    const [openMenu, setOpenMenu] = useState(false)
 
     
     useEffect(()=>{
@@ -72,73 +53,20 @@ export default function Navbar() {
             setShowDrawer(false)
         }
     },[downMd])
-
-    const [state, setState] = useState({
-        top: false,
-        left: false,
-        bottom: false,
-        right: false,
-      });
-
-    const toggleDrawer = (anchor, open) => (event) => {
-        if (
-          event &&
-          event.type === 'keydown' &&
-          (event.key === 'Tab' || event.key === 'Shift')
-        ) {
-          return;
-        }
-    
-        setState({ ...state, [anchor]: open });
-      };
-
       
     const onClickkNavbar = () => {
 
     }
 
-    const list = (anchor) => (
-        <Box
-          sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
-          role="presentation"
-          onClick={toggleDrawer(anchor, false)}
-          onKeyDown={toggleDrawer(anchor, false)}
-        >
-          <List>
-            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-              <ListItem key={text} disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                  </ListItemIcon>
-                  <ListItemText primary={text} />
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
-          <Divider />
-          <List>
-            {['All mail', 'Trash', 'Spam'].map((text, index) => (
-              <ListItem key={text} disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                  </ListItemIcon>
-                  <ListItemText primary={text} />
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
-        </Box>
-      );
+
 
     return(
 
     <Box className={styles.header}>
             <Image
               src="/images/logo.png" // Route of the image file
-              height={90} // Desired size with correct aspect ratio
-              width={106.25} // Desired size with correct aspect ratio
+              height={downMd ? 75 : 90} // Desired size with correct aspect ratio
+              width={downMd ? 88.5 : 106.25} // Desired size with correct aspect ratio
               alt="Analizarte"
               className={styles.logo}
             />
@@ -146,14 +74,31 @@ export default function Navbar() {
 
 showDrawer ? 
                 <>
-          <Button onClick={toggleDrawer("bottom", true)}>{"bottom"}</Button>
-          <Drawer
-            anchor={"bottom"}
-            open={state["bottom"]}
-            onClose={toggleDrawer("bottom", false)}
-          >
-            {list("bottom")}
-          </Drawer>
+
+                  <input checked={openMenu} onChange={()=>setOpenMenu(!openMenu)} type="checkbox" className={styles.navigation__checkbox} id="navi-toggle"/>
+
+                  <label htmlFor="navi-toggle" className={styles.navigation__button} style={{cursor:downSm ? "unset" : "pointer"}}>
+                      <span className={styles.navigation__icon} >&nbsp;</span>
+                  </label>
+                  {/*<div onClick={()=>setOpenMenu(!openMenu)} className={styles.menu} aria-label="Delete">
+                                <MenuIcon className={styles.menuIcon} sx={{fontSize:45}}/>
+            </div>*/}
+                  {
+                    !openMenu ? <></> : <>
+                    <div style={{position:"absolute", width:"100%", top:"6rem", left: "0", backgroundColor:"#CCD9F0", display:"flex", flexDirection:"column"}}>
+                        <div onClick={onClickkNavbar} style={{textAlign:"center"}} className={styles.menuItem}>Sección</div>
+                        <div onClick={onClickkNavbar} style={{textAlign:"center"}} className={styles.menuItem}>Sobre Mí</div>
+                        <div onClick={onClickkNavbar} style={{textAlign:"center"}} className={styles.menuItem}>Contacto</div>
+                        <div style={{textAlign:"center"}} className={styles.menuItem}>
+                          <Instagram className={styles.menuIcon} sx={{fontSize:30, marginRight:"0.7rem"}}/>
+                          <Call className={styles.menuIcon} sx={{fontSize:30, marginRight:"0.7rem"}}/>
+                          <MailOutline className={styles.menuIcon} sx={{fontSize:30}}/>
+
+                        </div>
+                    </div></>
+                  
+                  }
+
                 </>
                 :
                 <>
@@ -163,21 +108,21 @@ showDrawer ?
                         <p onClick={onClickkNavbar} className={styles.hoverUnderlineAnimation}>Contacto</p>
                     </div>
                     <div className={styles.socials}>
-                        <div style={{position:"absolute", width:"30rem", top:"-16rem", right:downLg ? "-9rem":"-6.5rem"}}>
+                        <div style={{zIndex: -10 ,position:"absolute", width:"30rem", top:"-16rem", right:downLg ? "-9rem":"-6.5rem"}}>
                             <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
                                 <path fill="#5364A7" d="M41.8,-73.7C53.2,-65.9,60.7,-52.6,67.5,-39.4C74.4,-26.2,80.6,-13.1,81.5,0.5C82.4,14.2,78.1,28.4,70.2,39.7C62.3,51.1,50.8,59.6,38.5,66.3C26.2,73,13.1,77.9,0.2,77.5C-12.7,77.2,-25.4,71.5,-39,65.6C-52.7,59.7,-67.4,53.6,-75.1,42.6C-82.7,31.6,-83.4,15.8,-83.8,-0.3C-84.3,-16.3,-84.5,-32.6,-77.3,-44.4C-70.1,-56.2,-55.5,-63.4,-41.3,-69.7C-27.2,-76,-13.6,-81.3,0.8,-82.7C15.2,-84.1,30.5,-81.6,41.8,-73.7Z" transform="translate(100 100)" />
                             </svg>
                         </div>
                         <ThemeProvider theme={theme}>
-                        <IconButton className={classes.customHoverFocus} color='primary' sx={{marginRight:"1rem"}} aria-label="Delete">
-                                <Instagram sx={{ fontSize: 45, color:"#CCD9F0" }} />
-                            </IconButton>
-                            <IconButton className={classes.customHoverFocus} color='primary' sx={{marginRight:"1rem"}}  aria-label="Delete">
-                                <Call  sx={{ fontSize: 45, color:"#CCD9F0" }} />
-                            </IconButton>
-                            <IconButton className={classes.customHoverFocus} color='primary' sx={{marginRight:"1rem"}}   aria-label="Delete">
-                                <MailOutline  sx={{ fontSize: 45, color:"#CCD9F0" }} />
-                            </IconButton>
+                        <div className={styles.customHoverFocus} aria-label="Delete">
+                                <Instagram className={styles.customIcon} sx={{fontSize:45}}/>
+                            </div>
+                            <div className={styles.customHoverFocus} aria-label="Delete">
+                                <Call className={styles.customIcon} sx={{fontSize:45}}/>
+                            </div>
+                            <div className={styles.customHoverFocus} aria-label="Delete">
+                                <MailOutline className={styles.customIcon} sx={{fontSize:45}}/>
+                            </div>
                         </ThemeProvider>
                     </div>
                 </>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,29 +16,6 @@ class MyDocument extends Document {
             content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
           />
           <meta name="theme-color" content={theme.palette.primary.main} />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons"
-          />
-          <style jsx global>
-            {`
-              html,
-              body {
-                height: 100%;
-                width: 100%;
-              }
-              *,
-              *:after,
-              *:before {
-                box-sizing: border-box;
-              }
-              body {
-                font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
-                font-size: 1rem;
-                margin: 0;
-              }
-            `}
-          </style>
         </Head>
         <body>
           <Main />
